4652319138ec20b7c08b6dea8db5b31378ffce35
Sabrine Barreto <sabrine0barreto@gmail.com>
2024-04-02 19:38:15 -0300

    First commit - Entity User e REST

---
+++ src/main/java/com/project/wsmongo/WsmongoApplication.java
@@ -0,0 +1,13 @@
+package com.project.wsmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/wsmongo/WsmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wsmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
