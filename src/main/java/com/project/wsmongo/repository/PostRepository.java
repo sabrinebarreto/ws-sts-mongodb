fefd2f1a7030702fef3189643c90e0adb8422c79
Sabrine Barreto <sabrine0barreto@gmail.com>
2024-04-10 18:33:44 -0300

    Consulta simples com @Query

--- src/main/java/com/project/wsmongo/repository/PostRepository.java
+++ src/main/java/com/project/wsmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.project.wsmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.project.wsmongo.domain.Post;
@@ -10,6 +11,10 @@ import com.project.wsmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	@Query("{'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle (String text);
+	
 	List<Post> findByTitleContainingIgnoreCase (String text);
+
 
 }
