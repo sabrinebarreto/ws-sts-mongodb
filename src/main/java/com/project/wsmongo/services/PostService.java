01619d727694ef5c063bcb2d8beb25c6780fc5c8
Sabrine Barreto <sabrine0barreto@gmail.com>
2024-04-10 17:55:09 -0300

    Obtendo um post por id

---
+++ src/main/java/com/project/wsmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.project.wsmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.wsmongo.domain.Post;
+import com.project.wsmongo.repository.PostRepository;
+import com.project.wsmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+
+}
