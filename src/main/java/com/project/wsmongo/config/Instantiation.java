9a7e11e5af623c277f2fe78d68ed2fd559864c36
Sabrine Barreto <sabrine0barreto@gmail.com>
2024-04-10 17:38:31 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/project/wsmongo/config/Instantiation.java
+++ src/main/java/com/project/wsmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.project.wsmongo.domain.Post;
 import com.project.wsmongo.domain.User;
+import com.project.wsmongo.dto.AuthorDTO;
 import com.project.wsmongo.repository.PostRepository;
 import com.project.wsmongo.repository.UserRepository;
 
@@ -34,8 +35,10 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP, abraços", maria);
-		Post post2= new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP, abraços", new AuthorDTO(maria));
+		Post post2= new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
